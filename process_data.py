a1045924c47e42a79c8b2796bbde5d376266d361
Lucas <lucas.weber@fau.de>
2026-05-08 10:20:19 +0200

    exclude magnetic sensor

--- process_data.py
+++ process_data.py
@@ -30,7 +30,7 @@ def process_data(input_tuple: tuple[str, str, int, str, str]):
 
     # load the data from the memory
     df = pd.read_parquet(data_path)
-    df = df.drop(['lat','lon','speed'], axis=1, errors='ignore')
+    df = df.drop(['lat','lon','speed', 'x-mag', 'y-mag', 'z-mag'], axis=1, errors='ignore')
 
     # transform to numpy
     signal = df.to_numpy()
